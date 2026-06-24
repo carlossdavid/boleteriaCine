cf82680fbdb8fe049c0e69ff5012ca3edada414d
Carlos Ortega <carlos.ortega01@epn.edu.ec>
2026-06-24 17:37:21 -0500

    Carga Inicial

---
+++ boleteriaCine/src/main/java/fis/proyect/boleteriacine/BoleteriaCine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.proyect.boleteriacine;
+
+/**
+ *
+ * @author carlos
+ */
+public class BoleteriaCine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
