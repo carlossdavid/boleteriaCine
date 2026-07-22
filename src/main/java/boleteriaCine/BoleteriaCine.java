799f23d2580881a4151659c4e759afed3df52a5b
carloss_david_ <carlos.ortega01@epn.edu.ec>
2026-07-22 12:19:02 -0500

    Reparacion Duplicados

--- src/main/java/boleteriaCine/BoleteriaCine.java
+++ src/main/java/boleteriaCine/BoleteriaCine.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package fis.proyect.boleteriacine;
+package boleteriaCine;
 
 import vista.InicioSesion;
 
